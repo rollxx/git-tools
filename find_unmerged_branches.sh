9712f831071a903f04d9b89a14b01593c74cce97
Thomas Birke <thomas.birke@netresearch.de>
2012-06-05 11:20:52 +0200

    add remote branches to be searched for unmerged commits

--- find_unmerged_branches.sh
+++ find_unmerged_branches.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-branches=`git branch | grep -v "\*"`
 currentBranch=`git branch | grep "\*" | sed "s/* //"`
+echo "Looking for branches not being merged to $currentBranch, yet"
+branches=`git branch -r | grep -v "\*" | grep -v "\->"`
 merged=""
 for branch in $branches; do
-    unmerged=`git cherry -v $currentBranch $branch|wc -l`
+    unmerged=`git cherry -v "$currentBranch" "$branch"|wc -l`
     if [ $unmerged -gt 0 ]; then
         echo $branch
-        git cherry -v $currentBranch $branch
+        git cherry -v "$currentBranch" "$branch"
         echo ""
     else
         merged="$merged $branch"
     fi
 done
 
-echo "Zu löschen:"
+echo "Alles gemergt:"
 for branch in $merged; do
     echo $branch
 done
